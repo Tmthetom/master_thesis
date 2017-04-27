3cc6c5cfaa327af3f9b42e193b4e7ca5ef642660
Tmthetom <moravec-tomas@seznam.cz>
2017-04-27 11:12:06 +0200

    Firmware updated with Get and Set

--- Firmware/Security/Security.ino
+++ Firmware/Security/Security.ino
@@ -1,7 +1,8 @@
 // Global variables
-int doorSenzors[] = {5, 6};               // Hold all door senzors
+int doors[] = {5, 6};                     // Hold all door senzors
 int leds[] = {2, 3, 4};                   // Hold all leds
 String string = "";                       // String to hold incoming data
+String separator = ",";                   // Data separator
 boolean stringComplete = false;           // Whether the string is complete
 
 // Initialization
@@ -11,11 +12,11 @@ void setup() {
   string.reserve(200);                    // Reserve 200 bytes for the inputString:
 
   // Init senzors
-  for (int thisPin = 0; thisPin < sizeof(doorSenzors); thisPin++) {
-    pinMode(doorSenzors[thisPin], INPUT);
+  for (int i = 0; i < sizeof(doors)/sizeof(int); i++) {
+    pinMode(doors[i], INPUT);
   }
-  for (int thisPin = 0; thisPin < sizeof(leds); thisPin++) {
-    pinMode(leds[thisPin], OUTPUT);
+  for (int i = 0; i < sizeof(leds)/sizeof(int); i++) {
+    pinMode(leds[i], OUTPUT);
   }
 }
 
@@ -39,13 +40,31 @@ void loop() {
       // Parse data
       String middle = string.substring(string.indexOf('[')+1);          // Get text after '['
       String data = middle.substring(0, middle.indexOf(']'));         // Get text before ']'
-      int separator = data.indexOf(';');                              // Get index of ';'
-      int pin = data.substring(0, separator).toInt();
-      int val = data.substring(separator+1).toInt();
+      int sep = data.indexOf(separator);                              // Get index of ';'
+      int pin = data.substring(0, sep).toInt();
+      int val = data.substring(sep+1).toInt();
 
       // Use data
       digitalWrite(pin, (val == 1) ? HIGH : LOW); 
       Serial.println("Pin[" + String(pin) + "] = [" + String(val) + "]");
+    }
+    else if (string.equalsIgnoreCase("GetLeds")){
+      String out = "Leds[";                                           // Add start of out
+      for (int i = 0; i < sizeof(leds)/sizeof(int); i++) {            // Foreach led
+        out += String(leds[i]) + ',';
+      }
+      out = out.substring(0, out.length()-1);                          // Cut last comma
+      out += ']';                                                     // Add end of out 
+      Serial.println(out);                                            // Send it
+    }
+    else if (string.equalsIgnoreCase("GetDoors")){
+      String out = "Doors[";                                          // Add start of out
+      for (int i = 0; i < sizeof(doors)/sizeof(int); i++) {           // Foreach door
+        out += String(doors[i]) + ',';
+      }
+      out = out.substring(0, out.length()-1);                           // Cut last comma
+      out += ']';                                                     // Add end of out 
+      Serial.println(out);                                            // Send it
     }
     else{
       Serial.println("No parse: " + string);
@@ -67,6 +86,7 @@ void serialEvent() {
     // If the incoming character is a newline, set a flag
     // So the main loop can do something about it:
     if (inChar == '\n') {
+      string.trim();
       stringComplete = true;
     }
   }
