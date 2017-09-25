e416b9f7c8f4dab06b74b2a8a8d5ee06cb0b79cd
Tomáš Moravec <moravec-tomas@seznam.cz>
2017-09-25 17:03:31 +0200

    Simplified Arduino Testing

--- Software/Security (Firmware)/Ostatní/TestArduino/TestArduino.ino
+++ Software/Security (Firmware)/Ostatní/TestArduino/TestArduino.ino
@@ -1,18 +1,14 @@
 #include <SoftwareSerial.h>
 
 SoftwareSerial gsm(10, 11); // RX, TX
-String arduinoReceived = "Arduino Received: ";
-String arduinoSending = "Arduino Sending: ";
+String arduinoReceived = "";
+String arduinoSending = "[-->] ";
 String received;
 
 void setup() {
   Serial.begin(9600);
-  while (!Serial) {
-    ;
-  }
-
-  Serial.println("Arduino Connected!");
   gsm.begin(9600);
+  Serial.println("Arduino Connected!");
 }
 
 void loop() {
