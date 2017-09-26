c740a0ae24ab48d7231d1cee38528651b3ec4fc3
Tomáš Moravec <moravec-tomas@seznam.cz>
2017-09-26 17:49:01 +0200

    Added all binary files (In case of not having VS)

---
+++ Software/Security (Firmware)/Security/Release/Security.cpp.d
@@ -0,0 +1,14 @@
+C:\Users\Tomáš\ Moravec\AppData\Local\Temp\VMBuilds\Security\uno\Release\Security.cpp.o: \
+ C:\Users\Tomáš\ Moravec\AppData\Local\Temp\VMBuilds\Security\uno\Release\Security.cpp \
+ E:\Programs\Arduino\hardware\arduino\avr\cores\arduino/arduino.h \
+ E:\Programs\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ E:\Programs\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ E:\Programs\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ E:\Programs\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ E:\Programs\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ E:\Programs\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ E:\Programs\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ E:\Programs\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ E:\Programs\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ E:\Programs\Arduino\hardware\arduino\avr\variants\standard/pins_arduino.h \
+ E:\Programs\Arduino\hardware\arduino\avr\libraries\EEPROM\src/EEPROM.h
