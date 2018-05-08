6f5002e0add295059021fb51c63d24659e0f3072
Tomáš Moravec <moravec-tomas@seznam.cz>
2018-05-08 14:29:26 +0200

    Fixed causing Arduino DUE was not working
    There was no support for Arduino DUE board, thats why sensors and switches at ports, not supported by default Arduino Uno, was not working correctly.

--- Software/SecurityControl (Desktopová aplikace)/SecurityControl/SecurityControl/Arduino/BoardPins.cs
+++ Software/SecurityControl (Desktopová aplikace)/SecurityControl/SecurityControl/Arduino/BoardPins.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SecurityControl.Arduino
 {
@@ -15,26 +11,103 @@ namespace SecurityControl.Arduino
         /// <returns>Array of pins</returns>
         public static string[] GetPins(string boardName)
         {
+            // For Arduino DUE "Native USB" and "Programming" ports
+            if (boardName.Contains("Arduino Due"))
+            {
+                return ArduinoDue();
+            }
+
+            // Other boards
             switch (boardName)
             {
                 case "Arduino Uno": return ArduinoDefault();
-
+                case "Genuino Uno": return ArduinoDefault();
+                
                 case "Arduino Mega": return ArduinoMega();
-                case "Arduino Mega 2560": return ArduinoMega();
-
                 case "Genuino Mega": return ArduinoMega();
+                
+                case "Arduino Mega 2560": return ArduinoMega();
                 case "Genuino Mega 2560": return ArduinoMega();
-
+                
                 default: return ArduinoDefault();
             }
         }
 
+        /// <summary>
+        /// Arduino Due
+        /// </summary>
+        /// <returns>List with numbers of pins</returns>
+        private static string[] ArduinoDue()
+        {
+            return new string[] {
+
+                // Digital pins
+                "2",
+                "3",
+                "4",
+                "5",
+                "6",
+                "7",
+                "8",
+                "9",
+                "10",
+                "11",
+                "12",
+                "13",
+                "22",
+                "23",
+                "24",
+                "25",
+                "26",
+                "27",
+                "28",
+                "29",
+                "30",
+                "31",
+                "32",
+                "33",
+                "34",
+                "35",
+                "36",
+                "37",
+                "38",
+                "39",
+                "40",
+                "41",
+                "42",
+                "43",
+                "44",
+                "45",
+                "46",
+                "47",
+                "48",
+                "49",
+                "50",
+                "51",
+                "52",
+                "53",
+
+                // Analog pins
+                "54",
+                "55",
+                "56",
+                "57",
+                "58",
+                "59",
+                "60",
+                "61",
+                "62",
+                "63",
+                "64",
+                "65"
+            };
+        }
 
         /// <summary>
         /// Arduino/Genuino Mega
         /// Arduino/Genuino Mega 2560
         /// </summary>
-        /// <returns></returns>
+        /// <returns>List with numbers of pins</returns>
         private static string[] ArduinoMega()
         {
             return new string[] {
@@ -95,7 +168,7 @@ namespace SecurityControl.Arduino
         /// Arduino/Genuino 101
         /// Severino
         /// </summary>
-        /// <returns></returns>
+        /// <returns>List with numbers of pins</returns>
         private static string[] ArduinoDefault()
         {
             return new string[] {
